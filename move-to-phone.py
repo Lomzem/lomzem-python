8ba9d06b13cab7711229d3377d5c14521af4da20
Lomzem <36811841+Lomzem@users.noreply.github.com>
2020-04-19 14:28:26 -0700

    Delete move-to-phone.py

--- move-to-phone.py
+++
@@ -1,8 +0,0 @@
-import pyperclip
-import sys
-
-if len(sys.argv) < 2:
-	print('ERROR: Open this script with a file!')
-else:
-	input_file = sys.argv[1]
-	pyperclip.copy(f'scp -r "{input_file}" root@10.0.0.200:/var/mobile/Documents/')
